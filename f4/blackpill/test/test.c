61c1a842c8b1b5ec520460cec1eaf7c53abdb177
AlexOrlov23 <alexorlov2000@gmail.com>
2025-05-23 00:58:56 +0300

    1

---
+++ f4/blackpill/test/test.c
@@ -0,0 +1,56 @@
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/f1/rcc.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/spi.h>
+#include <libopencm3/cm3/common.h>
+
+static void clock_setup(void)
+{
+        rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_48MHZ]);
+        rcc_periph_clock_enable(RCC_GPIOB);
+        rcc_periph_clock_enable(RCC_USART1);
+		rcc_periph_clock_enable(RCC_SDIO);
+}
+
+static void usart_setup(void)
+{
+	/* Setup GPIO6 (in GPIO port A) to 'output push-pull' for LED use. */
+	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
+	gpio_set_output_options(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO13);
+
+	
+
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9 | GPIO10);
+    gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO9);
+
+
+	
+	usart_set_baudrate(USART1, 115200);
+	usart_set_databits(USART1, 8);
+	usart_set_stopbits(USART1, USART_STOPBITS_1);
+	usart_set_mode(USART1, USART_MODE_TX);
+	usart_set_parity(USART1, USART_PARITY_NONE);
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+
+	
+	usart_enable(USART1);
+}
+
+int main(void)
+{
+
+
+	clock_setup();
+	usart_setup();
+
+	
+	while (1) {
+		gpio_toggle(GPIOC, GPIO13);	/* LED on/off */
+		usart_send(USART1, 'A');
+		for (int i = 0; i < 800000; i++)
+			__asm__("nop");
+	}
+
+	return 0;
+}
